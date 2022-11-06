226734370b686f35c601ef01d69b0f0347e2c3b8
charitety <caridadrobles@hotmail.com>
2022-11-06 16:54:51 -0500

    filtered data

--- analysis.py
+++ analysis.py
@@ -6,9 +6,18 @@ with open("edu-analysis/states_all.csv", "r") as infile:
     for row in reader:
         list_data.append(row)
 
-single_state = [row for row in list_data if row['STATE']== 'NEW YORK']
+#Obtaining the number of rows
+print(len(list_data))
 
-new_data = [row for row in single_state if row["FEDERAL_REVENUE"] != '']
+#Obtaining the number of columns or keys in data
+print(len(list_data[0]))
+
+#Selecting one state to analyze
+state_data = [row for row in list_data if row['STATE']=='NEW_YORK']
+print(len(state_data))
+
+new_data = [row for row in state_data if row['AVG_READING_4_SCORE']]
+print(len(new_data))
 """Potential fuctions
 get_highest_gdp(data, year):
 get_lowest_gdp(data, year):
