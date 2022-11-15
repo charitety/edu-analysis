6065a4c5a0cc2ad1ec2645394725c300bcb43f34
charitety <caridadrobles@hotmail.com>
2022-11-14 23:26:55 -0500

    continue extracting data and started fuction

--- analysis.py
+++ analysis.py
@@ -14,11 +14,36 @@ print(len(list_data[0]))
 
 #Selecting one state to analyze
 state_data = [row for row in list_data if row['STATE']=='NEW_YORK']
-print(len(state_data))
+print(state_data)
 
-new_data = [row for row in state_data if row['AVG_READING_4_SCORE']]
-print(len(new_data))
-"""Potential fuctions
-get_highest_gdp(data, year):
-get_lowest_gdp(data, year):
-get_state_gdp(data, state, year):"""
+#Choosing to analyze reading score for 4th graders and filtering the rows with no data
+state_data = [row for row in state_data if row['AVG_READING_4_SCORE'] != '']
+print(state_data)
+
+#Getting the value of YEAR by accessing the dictionary
+years = [(row['YEAR']) for row in state_data]
+print(years)
+
+#Definining a function to analyze percentage of change from one year to another
+def percent_change(data, year1, year2, column):
+    """
+    Calculate the percentage of change in state education
+    Parameters
+    ----------
+    data : list 
+        U.S. Education data between 1986 and 2019. Some years are missing.
+    year1 : str
+        Beginning year of analysis period
+    year2 : str
+        Final year of analysis period
+    column : str
+        contains scores per grade in a given category e.g. reading or math
+    
+    Returns
+    -------
+    percentage change : float
+
+    """
+
+
+
